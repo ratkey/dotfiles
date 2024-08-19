7ae91941299abdd52f46cbee8f159e78101b0b43
ratkey <cother@protonmail.com>
2024-08-19 11:32:08 -0700

    Added kitty, lf, nvim

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,11 @@
+return {
+  "stevearc/oil.nvim",
+  opts = {},
+  -- Optional dependencies
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  keys = {
+    -- Add the key mapping for opening the Oil plugin with "-"
+    { "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
+  },
+}
